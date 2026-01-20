6098ce1bbc915aa1d0d018ee17b1456add970929
KMYDALAGUAN <kmdalaguan@gmail.com>
2026-01-20 20:02:28 +0800

    Add files via upload

---
+++ lab2_student_record.py
@@ -0,0 +1,13 @@
+student = { 
+
+"id": "2025-001", 
+
+"name": "Kobi Dalaguan", 
+
+"grades": [88, 90, 85] 
+
+} 
+
+average = sum(student["grades"]) / len(student["grades"]) 
+
+print("Average:", average) 
